7bbf77e284329687028a6e8fef8fb33d7a24a8ba
STA <2179682+ajayita@users.noreply.github.com>
2026-05-05 00:27:57 -0500

    test: implement yaml fixture parser and models

---
+++ test/infrastructure/models.dart
@@ -0,0 +1,21 @@
+class ExpectedEvent {
+  final String type;
+  final List<int> bytes;
+  ExpectedEvent({required this.type, required this.bytes});
+}
+
+class TranscriptStep {
+  final List<int>? clientSends;
+  final List<ExpectedEvent>? expectEvents;
+  final Map<String, dynamic>? expectState;
+  
+  TranscriptStep({this.clientSends, this.expectEvents, this.expectState});
+}
+
+class TranscriptFixture {
+  final String name;
+  final String description;
+  final List<TranscriptStep> steps;
+
+  TranscriptFixture({required this.name, required this.description, required this.steps});
+}
